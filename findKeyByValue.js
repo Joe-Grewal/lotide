7af52a8ab9c2af30383fbf653e30210702547934
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-01 18:37:14 -0700

    Completed and tested function

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  const arrayOfKeys = Object.keys(object);
+  for (const keys of arrayOfKeys) {
+    if (object[keys] === value) {
+      return keys;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
