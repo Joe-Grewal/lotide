09247d71ba9faf6caa8a4e91f8256bbc995e75bc
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-13 17:38:58 -0800

    Refactored file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray)) {
     console.log(`✅✅✅Assertion Passed: ${firstArray} === ${secondArray}`);
@@ -6,27 +8,4 @@ const assertArraysEqual = function(firstArray, secondArray) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  let stringOfFirst = "";
-  let stringOfSecond = "";
-  for (let index1 = 0; index1 < array1.length; index1++) {
-    if (typeof array1[index1] === "string") {
-      stringOfFirst += array1[index1];
-    } else if (typeof array1[index1] !== "string") {
-      array1[index1].toString();
-      stringOfFirst += array1[index1];
-    }
-  }
-  for (let index2 = 0; index2 < array2.length; index2++) {
-    if (typeof array2[index2] === "string") {
-      stringOfSecond += array2[index2];
-    } else if (typeof array2[index2] !== "string") {
-      array2[index2].toString();
-      stringOfSecond += array2[index2];
-    }
-  } if (stringOfFirst === stringOfSecond) {
-    return true;
-  } else {
-    return false;
-  }
-};
+module.exports = assertArraysEqual;
