c733870af7e6db7048026c807a260f359cf094a0
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-21 16:26:04 -0800

    Fix eqArrays implementation for when one array doesn't contain strings

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,12 @@
 const eqArrays = function(array1, array2) {
-  let stringOfFirst = "";
-  let stringOfSecond = "";
-  for (let index1 = 0; index1 < array1.length; index1++) {
-    if (typeof array1[index1] === "string") {
-      stringOfFirst += array1[index1];
-    } else if (typeof array1[index1] !== "string") {
-      array1[index1].toString();
-      stringOfFirst += array1[index1];
-    }
-  }
-  for (let index2 = 0; index2 < array2.length; index2++) {
-    if (typeof array2[index2] === "string") {
-      stringOfSecond += array2[index2];
-    } else if (typeof array2[index2] !== "string") {
-      array2[index2].toString();
-      stringOfSecond += array2[index2];
-    }
-  } if (stringOfFirst === stringOfSecond) {
-    return true;
-  } else {
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let index = 0; index < array1.length; index++) {
+    if (array1[index] !== array2[index]) {
+      return false;
+    }
+  } return true;
 };
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -9,3 +9,5 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
